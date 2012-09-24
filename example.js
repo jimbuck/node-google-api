71986aa120776931521e6ff7c19ac1fc88dcba26
Jim Buck <jim@jimmyboh.com>
2012-09-24 00:07:02 -0400

    Updated `require` usage.

--- example.js
+++ example.js
@@ -1,39 +1,43 @@
-// psuedo-require while testing (not yet in npm...)
-var gapi = require('./lib/node-google-api');
+
+var key = '<<YOUR KEY HERE>>',
+var apiLib = require('./lib/node-google-api')(key);
 var url = require('url');
 
-gapi.list({name:'calendar'}, function(list) {
-	if(list)  {
-		switch(list.length) {
-			case 0:
-				console.log("No APIs found!");
-				break;
-			case 1:
-				gapi.getRest(list[0], function(api) {
-					var path = api.resources.events.methods.list.path+'?key=<<YOUR KEY HERE>>';
-					path = path.replace('{calendarId}',encodeURIComponent('en.usa#holiday@group.v.calendar.google.com'));
-					var eventUrl = url.parse(api.baseUrl + path);
-					
-					gapi.request({
-						host: eventUrl.hostname,
-						path: eventUrl.path,
-						port: eventUrl.port,
-						method: 'GET'
-					}, console.log);
-				});
-				break;
-			default:
-				console.log(list);
-				break;
+var options = {
+	apiKey: key,
+	calendarId: 'en.usa#holiday@group.v.calendar.google.com'
+}
+
+apiLib.build(function(gapi) {
+	gapi.listAPIs({name:'calendar'}, function(list) {
+		if(list)  {
+			switch(list.length) {
+				case 0:
+					console.log("No APIs found!");
+					break;
+				case 1:
+					gapi.getAPI(list[0], function(api) {
+						var path = api.resources.events.methods.list.path+'?key='+options.apiKey;
+						path = path.replace('{calendarId}',encodeURIComponent(options.calendarId));
+						var eventUrl = url.parse(api.baseUrl + path);
+						
+						gapi.request({
+							host: eventUrl.hostname,
+							path: eventUrl.path,
+							port: eventUrl.port,
+							method: 'GET'
+						}, console.log);
+					});
+					break;
+				default:
+					console.log(list);
+					break;
+			}
+		} else {
+			console.log("No list was found!");
 		}
-		if(list.length > 0) {
-			
-			
-		}
-	} else {
-		console.log("No APIs found!");
-	}
-	//*/
+		//*/
+	});
 });
 
 
