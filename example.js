df82746fcc490df68747a48e6342ebdd3dc87504
Jim Buck <jim@jimmyboh.com>
2012-09-24 13:40:29 -0400

    APIs are now built and accessible.
    If you haven't already, visit https://code.google.com/apis/console to
    grab your API key.

--- example.js
+++ example.js
@@ -3,41 +3,15 @@ var key = '<<YOUR KEY HERE>>',
 var apiLib = require('./lib/node-google-api')(key);
 var url = require('url');
 
-var options = {
-	apiKey: key,
-	calendarId: 'en.usa#holiday@group.v.calendar.google.com'
-}
-
 apiLib.build(function(gapi) {
-	gapi.listAPIs({name:'calendar'}, function(list) {
-		if(list)  {
-			switch(list.length) {
-				case 0:
-					console.log("No APIs found!");
-					break;
-				case 1:
-					gapi.getAPI(list[0], function(api) {
-						var path = api.resources.events.methods.list.path+'?key='+options.apiKey;
-						path = path.replace('{calendarId}',encodeURIComponent(options.calendarId));
-						var eventUrl = url.parse(api.baseUrl + path);
-						
-						gapi.request({
-							host: eventUrl.hostname,
-							path: eventUrl.path,
-							port: eventUrl.port,
-							method: 'GET'
-						}, console.log);
-					});
-					break;
-				default:
-					console.log(list);
-					break;
-			}
-		} else {
-			console.log("No list was found!");
+	var today = new Date();
+	gapi.calendar.events.list({
+		calendarId: 'en.usa#holiday@group.v.calendar.google.com'
+	}, function(eventList) {
+		for(var e in eventList.items) {
+			if(new Date(eventList.items[e].start.date) > today)
+				console.log(eventList.items[e].summary + ': '+eventList.items[e].start.date);
 		}
-		//*/
 	});
+	
 });
-
-
